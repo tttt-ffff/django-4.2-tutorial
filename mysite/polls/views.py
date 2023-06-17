74b7add4a913c4ddd2749229ad57b546443eaeb0
tttt-ffff <realmoon34@gmail.com>
2023-06-17 12:10:49 +0900

    投票結果をPOSTしてresultにリダイレクトする

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-#from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 #from django.template import loader
 
 from .models import Question
@@ -17,10 +18,24 @@ def detail(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   return render(request, "polls/detail.html", {"question": question})
 
-#def results(request, question_id):
-#  response = "You're looking at results of question %s."
-#  return HttpResponse(response % question_id)
-#
-#def vote(request, question_id):
-#  return HttpResponse("You're voting on question %s." % question_id)
+def results(request, question_id):
+  response = "You're looking at results of question %s."
+  return HttpResponse(response % question_id)
 
+def vote(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST["choice"])
+  except (KeyError, Choice.DoesNotExist):
+    return render(
+      request,
+      "polls/detail.html",
+      {
+        "question": question,
+        "error_message": "You didn't select a choice.",
+      },
+    )
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
